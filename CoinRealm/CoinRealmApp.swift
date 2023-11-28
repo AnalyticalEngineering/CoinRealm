370269bbd082d413391317bc2fa4e0bf3005af8a
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-11-28 16:25:32 -0600

    Initial Commit

---
+++ CoinRealm/CoinRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRealmApp.swift
+//  CoinRealm
+//
+//  Created by J. DeWeese on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
